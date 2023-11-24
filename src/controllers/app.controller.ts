3b53d8b15d7428b5c4d1fe56fcbf1caa9af58aeb
tarek saimouah <tareksaimouah@gmail.com>
2023-11-24 20:02:44 +0300

    added changes

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -3,6 +3,6 @@ import config from '../config'
 
 export default class AppController {
   hello(req: Request, res: Response) {
-    res.status(200).send(`Hello from ${config.NODE_ENV}!`)
+    res.status(200).send(`Hello from ${config.NODE_ENV} server!`)
   }
 }
